001d60947b60aa02d90f388abcbabfb02c95fe72
Wes Davis <wesley.r.davis@gmail.com>
2021-03-06 00:31:28 -0500

    UPD: Transform formatting

--- ZenPacks/daviswr/NCPA/transforms/Status/NCPA/class.py
+++ ZenPacks/daviswr/NCPA/transforms/Status/NCPA/class.py
@@ -3,9 +3,5 @@ from ZenPacks.daviswr.NCPA.lib.ncpaUtil import service_states
 service_strings = dict(map(reversed, service_states.items()))
 
 if evt.eventKey.endswith('|Service Status'):
-    status = service_strings.get(
-        int(float(evt.current)),
-        'unknown'
-        )
-
+    status = service_strings.get(int(float(evt.current)), 'unknown')
     evt.summary = '{0} service is {1}'.format(evt.component, status)
