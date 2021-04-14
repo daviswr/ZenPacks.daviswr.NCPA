d2f33ae9120502a6f03fc642090875057f3efd6c
Wes Davis <wesley.r.davis@gmail.com>
2021-04-13 23:09:41 -0400

    UPD: ZenPack metadata in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.daviswr.NCPA"
-VERSION = "0.0.1-dev"
+VERSION = "1.0.0"
 AUTHOR = "Wes Davis"
-LICENSE = "MIT"
+LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.daviswr']
 PACKAGES = ['ZenPacks', 'ZenPacks.daviswr', 'ZenPacks.daviswr.NCPA']
 INSTALL_REQUIRES = ['ZenPacks.zenoss.ZenPackLib', 'ZenPacks.zenoss.HttpMonitor', 'ZenPacks.zenoss.PythonCollector']
